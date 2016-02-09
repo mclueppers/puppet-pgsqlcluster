2e66cafdf692c6a794dc3f5d5e7947cb935e41f9
Martin Dobrev <martin.dobrev@betgenius.com>
2016-02-09 09:32:45 +0000

    Added SSL configuration

--- manifests/init.pp
+++ manifests/init.pp
@@ -35,7 +35,62 @@
 #
 # Copyright 2016 Your name here, unless otherwise noted.
 #
-class pgsqlcluster {
+# lint:ignore:autoloader_layout lint:ignore:80chars
 
+class pgsqlcluster (
+  $server_type = 'master',
+  $username    = undef,
+  $password    = undef,
 
+) inherits ::pgsqlcluster::params { # lint:ignore:class_inherits_from_params_class
+  file { '/var/lib/pgsql/data/server.crt':
+    ensure => 'present',
+    source => "file:///var/lib/puppet/ssl/certs/${::fqdn}.pem",
+    mode   => '0400',
+    owner  => 'postgres',
+    group  => 'postgres'
+  } ->
+  file { '/var/lib/pgsql/data/server.key':
+    ensure => 'present',
+    source => "file:///var/lib/puppet/ssl/private_keys/${::fqdn}.pem",
+    mode   => '0400',
+    owner  => 'postgres',
+    group  => 'postgres'
+  } ->
+  file { '/var/lib/pgsql/data/root.crt':
+    ensure => 'present',
+    source => 'file:///var/lib/puppet/ssl/certs/ca.pem',
+    mode   => '0400',
+    owner  => 'postgres',
+    group  => 'postgres'
+  } ->
+  postgresql::server::config_entry { 'wal_level':
+    value => 'hot_standby'
+  } ->
+  postgresql::server::config_entry { 'max_wal_senders':
+    value => 3
+  } ->
+  postgresql::server::config_entry { 'checkpoint_segments':
+    value => 8
+  } ->
+  postgresql::server::config_entry { 'wal_keep_segments':
+    value => 8
+  } ->
+  postgresql::server::config_entry { 'ssl':
+    value => 'on'
+  } ->
+  postgresql::server::pg_hba_rule { 'allow slave server to access master':
+    description => 'Open up PostgreSQL for access from 10.0.0.0/8',
+    type        => 'hostssl',
+    database    => 'all',
+    user        => $username,
+    address     => '10.0.0.0/8',
+    auth_method => 'md5',
+  } ->
+  postgresql::server::role { $username:
+    password_hash => postgresql_password($username, $password),
+    replication   => true,
+    require       => Class['::postgresql::server']
+  }
 }
+# lint:endignore
